16916b185ffb53db8e930af6e65cba35639c3354
Christopher Brembach <Christopher.Brembach@gmail.com>
2019-07-07 16:14:21 +0200

    Update P6_190523_LedFeuer.ino

--- P6_190523_LedFeuer.ino
+++ P6_190523_LedFeuer.ino
@@ -1,22 +1,37 @@
+//Code teilweise aus FastLed Fire Example übernommen und für unsere Zwecke angepasst
+
 #include <FastLED.h>
+#include <IRremote.h>
+#include <IRremoteInt.h>
 
 #define LED_PIN     5
-#define COLOR_ORDER GBR
+#define COLOR_ORDER GRB
 #define CHIPSET     WS2812B
-#define NUM_LEDS   30
+#define NUM_LEDS    150
 
 
-#define BRIGHTNESS  254
-#define FRAMES_PER_SECOND 100
+#define BRIGHTNESS 245
+#define FRAMES_PER_SECOND 60
+int m = 0;
+int RECV_PIN = 7;
+int NUM_LED=150;
+//int laserPin = 8;
+
+// Arduino-IRremote library will be initialized
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 
 bool gReverseDirection = false;
 
 CRGB leds[NUM_LEDS];
 
+
 void setup() {
   delay(3000); // sanity delay
-  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LED).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness( BRIGHTNESS );
+  irrecv.enableIRIn();
+  Serial.begin(9600);
 }
 
 void loop()
@@ -67,21 +82,88 @@ void loop()
 // SPARKING: What chance (out of 255) is there that a new spark will be lit?
 // Higher chance = more roaring fire.  Lower chance = more flickery fire.
 // Default 120, suggested range 50-200.
-#define SPARKING 10
+#define SPARKING 120
 
 
 void Fire2012()
 {
+  if (irrecv.decode(&results)) {
+    Serial.println(m);
+    
+    m = m + 1;
+    
+    //if (m > 5) {
+       // NUM_LED=149;
+       //Serial.println("its5");
+      //}
+      
+       
+      
+    if (m == 5) {
+        NUM_LED=150;
+        FastLED.clear ();
+       Serial.println("its5");
+      }
+      if (m == 10) {
+      NUM_LED=120;
+      FastLED.clear ();
+      Serial.println("its10");
+      }
+      if (m == 15) {
+      NUM_LED=100; 
+      FastLED.clear ();
+      Serial.println("its15");
+      }
+      if (m == 20) {
+      NUM_LED=80; 
+      FastLED.clear ();
+      Serial.println("its15");
+      }
+      if (m == 25) {
+      NUM_LED=60; 
+      FastLED.clear ();
+      Serial.println("its15");
+      }
+      if (m == 30) {
+      NUM_LED=40;
+      Serial.println("its20");
+      FastLED.clear ();
+      }
+      if (m == 33) {
+      NUM_LED=20; 
+      FastLED.clear ();
+      Serial.println("its15");
+      
+      }
+      if (m == 35) {
+      NUM_LED=1; 
+      FastLED.clear ();
+      Serial.println("its15");
+      m = 1;
+      m = m + 1;
+      }
+    //if (results.value == 0xFF02FD){
+    //hash: D7E84B1B
+    //real: FF02FD
+    //At signal input, the received and decoded signal will show via serial console.
+    Serial.println(results.value, HEX);
+    //i= i+1;
+    //Serial.println(i);
+    irrecv.resume();
+  }
+    
+    
+
 // Array of temperature readings at each simulation cell
   static byte heat[NUM_LEDS];
 
   // Step 1.  Cool down every cell a little
-    for( int i = 0; i < NUM_LEDS; i++) {
-      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    for( int i = 0; i < NUM_LED; i++) {
+      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LED) + 2));
     }
   
     // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-    for( int k= NUM_LEDS - 1; k >= 2; k--) {
+    for( int k= NUM_LED - 1; k >= 2; k--) {
       heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
     }
     
@@ -92,11 +174,11 @@ void Fire2012()
     }
 
     // Step 4.  Map from heat cells to LED colors
-    for( int j = 0; j < NUM_LEDS; j++) {
+    for( int j = 0; j < NUM_LED; j++) {
       CRGB color = HeatColor( heat[j]);
       int pixelnumber;
       if( gReverseDirection ) {
-        pixelnumber = (NUM_LEDS-1) - j;
+        pixelnumber = (NUM_LED-1) - j;
       } else {
         pixelnumber = j;
       }
